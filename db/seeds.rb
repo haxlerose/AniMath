7dd7957f0b395f749c823c1d4e5d853b0804865c
Paul Chesterton <chestertonpa@gmail.com>
2025-03-04 16:57:05 -0500

    Add some test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  { name: 'frog', level: 1, group: :amphibian, habitat: :land },
+  { name: 'spider', level: 2, group: :arachnid, habitat: :land },
+  { name: 'eagle', level: 3, group: :bird, habitat: :air },
+  { name: 'shark', level: 4, group: :fish, habitat: :sea },
+  { name: 'ant', level: 5, group: :insect, habitat: :land },
+  { name: 'tiger', level: 1, group: :mammal, habitat: :land },
+  { name: 'snake', level: 2, group: :reptile, habitat: :land },
+  { name: 'whale', level: 3, group: :mammal, habitat: :sea },
+  { name: 'octopus', level: 4, group: :mammal, habitat: :sea },
+  { name: 'penguin', level: 5, group: :bird, habitat: :land }
+].each do |animal|
+  Animal.find_or_create_by!(animal)
+end
+
+if Rails.env.development?
+  Game.find_or_create_by!(name: 'foobar')
+end
