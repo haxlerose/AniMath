4d89ce61ddd6718d35b0f5fd84308de26e383ccf
Paul Chesterton <chestertonpa@gmail.com>
2025-03-06 11:40:18 -0500

    make seeds.rb file use CSV data for importing animals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
 puts 'Seeding database...'
-[
-  { name: 'frog', level: 1, group: :amphibian, habitat: :land },
-  { name: 'spider', level: 2, group: :arachnid, habitat: :land },
-  { name: 'eagle', level: 3, group: :bird, habitat: :air },
-  { name: 'shark', level: 4, group: :fish, habitat: :sea },
-  { name: 'ant', level: 5, group: :insect, habitat: :land },
-  { name: 'tiger', level: 1, group: :mammal, habitat: :land },
-  { name: 'snake', level: 2, group: :reptile, habitat: :land },
-  { name: 'whale', level: 3, group: :mammal, habitat: :sea },
-  { name: 'octopus', level: 4, group: :mammal, habitat: :sea },
-  { name: 'penguin', level: 5, group: :bird, habitat: :land }
-].each do |animal|
-  Animal.find_or_create_by!(animal)
+CSV.foreach("db/data/animals.csv", headers: true) do |row|
+  Animal.create!(
+    name: row["Name"],
+    level: row["Level"],
+    habitat: row["Habitat"],
+    group: row["Group"]
+  )
 end
 
 puts "#{Animal.count} animals created!"
