b4725cc0cd67ad90c16d527bc6df92e1b3f0dbf6
Paul Chesterton <chestertonpa@gmail.com>
2025-03-07 10:49:49 -0500

    remove n+1 query

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,7 +3,9 @@
 class AnimalsController < ApplicationController
   def index
     @game = Game.find(params[:id])
-    @animals = Animal.all.group_by(&:level).sort_by { |key, _| key }.to_h.transform_values { |animals| animals.sort_by(&:name) }
-    @owned_animals = @game.animals
+    @animals = Animal.all.group_by(&:level).sort_by do |key, _|
+      key
+    end.to_h.transform_values { |animals| animals.sort_by(&:name) }
+    @owned_animals = @game.animals.to_a
   end
 end
