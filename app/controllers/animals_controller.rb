9097162269ea1c09772d5c7c1471f47f8753ce6f
Paul Chesterton <chestertonpa@gmail.com>
2025-03-10 13:21:33 -0400

    remove turbo frames

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,9 +3,8 @@
 class AnimalsController < ApplicationController
   def index
     @game = Game.find(params[:id])
-    @animals = Animal.all.group_by(&:level).sort_by do |key, _|
-      key
-    end.to_h.transform_values { |animals| animals.sort_by(&:name) }
+    grouped_animals = Animal.all.group_by(&:level)
+    @animals = grouped_animals.sort_by { |key, _| key }.to_h.transform_values { |animals| animals.sort_by(&:name) }
     @owned_animals = @game.animals.to_a
   end
 end
